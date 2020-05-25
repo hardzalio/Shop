e702f7f2e1ef1f749267fb2b0b5e37847e0089fd
alexprodacsion <65677984+alexprodacsion@users.noreply.github.com>
2020-05-25 14:56:43 +0200

    -formupdate
    -updated files

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,8 @@
-
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
+
 
 import { AlertService, AuthenticationService } from '../_services';
 
